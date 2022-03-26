3a484fec593b60e3935ed710fb4437ce0300d22c
Tyler Duran <tymduran@gmail.com>
2022-03-26 11:45:45 -0700

    Initial Commit

---
+++ loop_challenges.js
@@ -0,0 +1,42 @@
+/***********************/
+    // 1. Print odds 1-20
+
+for(i=1; i < 20; i++){
+    if(i % 2 === 1){
+        console.log(i)
+    }
+}
+
+/***********************/
+    // 2. Decreasing Multiples of 3
+
+for(i = 100; i > 0; i--) {
+    if(i % 3 == 0) {
+        console.log(i)
+    }
+}
+
+/***********************/
+    // 3. Print the sequence
+
+for(i = 4; i >= -3.5; i-=1.5){
+    console.log(i)
+}
+
+/***********************/
+    // 4. Sigma
+
+var sum = 0;
+for(i = 1; i <= 100; i++){
+    sum = sum + i;
+}
+console.log(sum)
+
+/***********************/
+    // 5. Factorial
+
+var product = 0
+for(i = 0; i <=12; i++){
+    product = i*i;
+    console.log(product)
+}
